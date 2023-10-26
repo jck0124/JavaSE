e34e25d82d97f8923a71e10d746253485ea90b9f
CHEN11122233444 <chan7579@naver.com>
2023-10-26 16:04:06 +0900

    section05

---
+++ src/section05/HomeWork01.java
@@ -0,0 +1,87 @@
+package section05;
+
+public class HomeWork01 {
+	public static void main(String[] args) {
+		for(int i=0;i<4;i++) {
+			for(int j=3;j>0;j--) {
+				if(j>i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println("*");
+		}
+		
+		
+		System.out.println("===============");
+		
+		
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<3-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<i+1;j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		
+		System.out.println("================");
+		
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<3-i;j++) {
+				// 4, 3, 2, 1
+				//System.out.print("o");
+				System.out.print(" ");
+			}
+			for(int j=0;j<2*i+1;j++) {
+				// 1, 3, 5 ,7
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("======================");
+		
+		for(int i=0;i<7;i++) {
+			if(i<4) {
+				// i=0,1,2,3
+				for(int j=0;j<3-i;j++) {
+					// 3 2 1 0
+					System.out.print(" ");
+				}
+				for(int j=0;j<2*i+1;j++) {
+					// 1 3 5 7
+					System.out.print("*");
+				}
+				System.out.println();
+			}else if(i>=4){
+				// i=4,5,6
+				for(int j=0;j<i-3;j++) {
+					// 1 2 3
+					System.out.print(" ");
+				}
+				for(int j=0;j<(13-2*i);j++) {
+					// 5 3 1
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+		
+		System.out.println("=====================");
+		
+		
+		
+		
+		
+
+		
+		
+		
+	}
+}
